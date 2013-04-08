b1d472135e688a4264b3f6f768085ef426b2fb9f
Horizon <m_horizon@live.com>
2013-04-08 17:38:21 +0430

    myfiles

---
+++ main2.java
@@ -0,0 +1,8 @@
+//import javax.swing.JOptionPane;
+public class main2 {
+	public static void main(String args[]){
+matrix a1=new matrix(2,true);
+matrix a2=new matrix(2,true);
+a1.matmul(a2).print();
+}
+}
